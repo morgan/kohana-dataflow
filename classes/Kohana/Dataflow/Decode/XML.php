64ab713b9fcb29f8cf1e3f85d3013f46932c3e65
Micheal Morgan <micheal@morgan.ly>
2013-07-23 21:45:01 -0400

    Resolved issue decoding XML attributes.

--- classes/Kohana/Dataflow/Decode/XML.php
+++ classes/Kohana/Dataflow/Decode/XML.php
@@ -98,6 +98,11 @@ class Kohana_Dataflow_Decode_XML extends Dataflow_Decode
 			);
 			
 			$this->_decoded =& $this->_decoded[$index];
+
+			if ( ! empty($attributes))
+			{
+				$this->_decoded[$this->_config[':attributes']] = $attributes;
+			}
 		}
 	}
 	
